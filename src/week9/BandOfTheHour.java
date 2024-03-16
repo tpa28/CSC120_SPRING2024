611e9402b219b659e0cca7bdc6b3a5afd4dea27d
tpa28 <tpa29@miami.edu>
2024-03-16 15:04:04 -0400

    BandOfTheHour code

--- src/week9/BandOfTheHour.java
+++ src/week9/BandOfTheHour.java
@@ -42,81 +42,98 @@ public class BandOfTheHour {
      */
     private static int numberOfRows;
 
-//-----------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------
     /**
      Default constructor.
      */
     public BandOfTheHour() {
         initializeStadium();
     }
-//-----------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------
     /**
-     * Initializes the stadium by prompting the user to input the number of rows and positions in each row.
+     * Initializes the stadium by prompting the user to input the number of rows
+     * and positions in each row.
      */
     private static void initializeStadium() {
+        // Variables to hold system values
+        int numberOfPositions;
+        char rowLetter;
+
         System.out.println("Welcome to the Band of the Hour");
         System.out.println("-------------------------------");
         System.out.printf("Please enter the number of rows               : ");
+
+        /** Input validation for number of rows */
         do {
             numberOfRows = keyboard.nextInt();
             if (numberOfRows <= 0 || numberOfRows > MAX_ROWS) {
                 System.out.print("ERROR: Out of range, try again                : ");
-            }
+            } // end of if statement
         } while (numberOfRows <= 0 || numberOfRows > MAX_ROWS);
 
-
         weightAssignments = new double[numberOfRows][];
+
+        // The for loop will allow an input for the number of positions per rows
         for (int index = 0; index < numberOfRows; index++) {
-            int numberOfPositions;
-            char rowLetter = (char) ('A' + index);
+            rowLetter = (char) ('A' + index);
 
             System.out.print("Please enter the number of positions in row " + rowLetter + " : ");
             do {
+                /** Input validation for whther the input is a positive integer */
                 while (!keyboard.hasNextInt()) {
                     System.out.print("ERROR: Invalid input. Please enter a valid integer.");
-                    keyboard.next(); // consume invalid input
-                }
+                    keyboard.next();
+                } // end of while loop
                 numberOfPositions = keyboard.nextInt();
 
                 if (numberOfPositions <= 0 || numberOfPositions > MAX_POSITIONS) {
                     System.out.print("ERROR: Out of range, try again                : ");
-                }
+                } // end of if statement
             } while (numberOfPositions <= 0 || numberOfPositions > MAX_POSITIONS);
 
             weightAssignments[index] = new double[numberOfPositions];
-        }
-    }
-//-----------------------------------------------------------------------------
+        } // end of the for loop
+
+    } // end of initializeStadium method
+
+    //-----------------------------------------------------------------------------
     /**
      * Displays the main menu options.
      */
     private static void printMenu() {
         System.out.print("\n(A)dd, (R)emove, (P)rint,              e(X)it : ");
-    }
-//-----------------------------------------------------------------------------
+    } // end of printMenu method
+
+    //-----------------------------------------------------------------------------
     /**
      * Prints the current assignment of musicians in the stadium, along with total and average weights for each row.
      */
     private static void printAssignment() {
+        // Variables to hold system values
         double totalWeight, averageWeight;
         int totalPositions;
 
+        // The for loop will check each row and find the total positions, then print the row letter
         for (int index = 0; index < numberOfRows; index++) {
             totalWeight = 0.0;
             totalPositions = weightAssignments[index].length;
-
             System.out.print((char) ('A' + index) + ":   ");
+
+            // The for loop will check each position within the row, print the weight of
+            // the musician in that current position, and
+            // calculate the total weight of musicians in the row
             for (int columns = 0; columns < totalPositions; columns++) {
                 System.out.print(weightAssignments[index][columns] + "   ");
                 totalWeight += weightAssignments[index][columns];
-            }
+            } // end of the for loop
 
             averageWeight = (totalPositions > 0) ? totalWeight / totalPositions : 0.0;
             System.out.println("                        [    " + totalWeight + ",     " + averageWeight + "]");
-        }
+        } // end of the nested for loop
 
-    }
-//-----------------------------------------------------------------------------
+    } // end of printAssignment method
+
+    //-----------------------------------------------------------------------------
     /**
      * Adds a musician to the specified row and position, checking for weight limits.
      * Prompts the user to enter the row letter and position number of the musician to be added.
@@ -126,58 +143,61 @@ public class BandOfTheHour {
      * @see BandOfTheHour
      */
     private static void addMusician() {
+        // Variables to hold system values
         int position, totalPositions;
         double weight, rowTotalWeight;
         char rowLetter;
 
         System.out.print("Please enter row letter                       : ");
         rowLetter = keyboard.next().toUpperCase().charAt(0);
-
+        /** Input validation for whether that row exists or if the input is wrong */
         while (rowLetter < 'A' || rowLetter > ('A' + numberOfRows - 1)) {
             System.out.print("ERROR: Out of range, try again                : ");
             rowLetter = keyboard.next().toUpperCase().charAt(0);;
-        }
+        } // end of the while loop
         totalPositions = weightAssignments[rowLetter - 'A'].length;
 
         System.out.print("Please enter position number (1 to " + totalPositions + ")         : ");
         position = keyboard.nextInt();
+        /** Input validation for choosing a position in a row */
         while (position < 1 || position > totalPositions) {
                 System.out.print("ERROR: Out of range, try again                : ");
                 position = keyboard.nextInt();
-        }
+        } // end of the while loop
 
+        // Checking if there is already a musician at that position */
         if (weightAssignments[rowLetter - 'A'][position - 1] != 0.0) {
             System.out.println("ERROR: There is already a musician there.");
             return;
-        }
+        } // end of if statement
 
         System.out.print("Please enter weight (" + MIN_WEIGHT + " to " + MAX_WEIGHT + ")           : ");
         weight = keyboard.nextDouble();
-
+        /** Input validation for weight at that position */
         while (weight < MIN_WEIGHT || weight > MAX_WEIGHT) {
             System.out.print("ERROR: Out of range, try again                : ");
             weight = keyboard.nextDouble();
-        }
+        } // end of the while loop
 
         rowTotalWeight = 0.0;
 
-        /**
-         * This loop iterates over each weight (w) in the array weightAssignments[rowLetter - 'A'],
-         * where rowLetter - 'A' is used to get the index of the row corresponding to the input rowLetter.
-         * Each w represents the weight of a musician in the specified row.
-         * */
+        // This for loop iterates over each musicianWeight at each postion in weightAssignments[rowLetter - 'A'],
+        //  to find the total weight within that row
         for (double musicianWeight : weightAssignments[rowLetter - 'A']) {
             rowTotalWeight += musicianWeight;
-        }
+        } // end of the for loop
+
+        // Checking if the new musician wiil make the row overweight
         if (rowTotalWeight + weight > (totalPositions*100)) {
             System.out.println("ERROR: Adding this musician would exceed the row weight limit.");
             return;
-        }
+        } // end of if statement
 
         weightAssignments[rowLetter - 'A'][position - 1] += weight;
         System.out.println("****** Musician added.");
-    }
-//-----------------------------------------------------------------------------
+    } // end of addMusician method
+    //-----------------------------------------------------------------------------
+
     /**
      * Removes a musician from the stadium seating.
      * Prompts the user to enter the row letter and position number of the musician to be removed.
@@ -188,35 +208,41 @@ public class BandOfTheHour {
      * @see BandOfTheHour
      */
     private static void removeMusician() {
+        // Variables to hold system values
         int position, totalPositions;
         char rowLetter;
 
         System.out.print("Please enter row letter                       : ");
         rowLetter = keyboard.next().toUpperCase().charAt(0);
 
+        // Checking if that row letter exists
         if (rowLetter < 'A' || rowLetter > ('A' + numberOfRows - 1)) {
             System.out.print("ERROR: Out of range, try again:");
             rowLetter = keyboard.next().toUpperCase().charAt(0);
-        }
+        } // end of if statement
+
         totalPositions = weightAssignments[rowLetter - 'A'].length;
 
         System.out.print("Please enter position number (1 to " + totalPositions + ")         : ");
         position = keyboard.nextInt();
 
+        // Checking if there is such position within that row
         if (position < 1 || position > totalPositions) {
             System.out.print("ERROR: Out of range, try again                : ");
             position = keyboard.nextInt();
-        }
+        } // end of if statement
 
+        // Checking if there is a musician at that position
         if (weightAssignments[rowLetter - 'A'][position - 1] == 0.0) {
             System.out.println("ERROR: That position is vacant.");
             return;
-        }
+        } // end of if statement
 
         weightAssignments[rowLetter - 'A'][position - 1] = 0.0;
         System.out.println("****** Musician removed.");
-    }
-//-----------------------------------------------------------------------------
+    } // end of removeMusician method
+
+    //-----------------------------------------------------------------------------
     /**
      * The main method of the BandOfTheHour class.
      * Instantiates a BandOfTheHour object and repeatedly displays the menu, prompts the user for input,
@@ -225,23 +251,25 @@ public class BandOfTheHour {
      * @param args to run main method
      */
     public static void main(String[] args) {
+        // Variables to hold system values
+        char option;
         BandOfTheHour bandOfTheHour = new BandOfTheHour();
 
-        char option;
         option = ' ';
 
         do {
-
             printMenu();
             String optionInput = keyboard.next().toUpperCase();
 
+            // Checking if optionInput contains A, P, R, or X
             if (!optionInput.matches("[ARPX]")) {
                 System.out.print("ERROR: Invalid option, try again              : ");
                 optionInput = keyboard.next();
-            }
+            } // end of if statement
 
             option = optionInput.charAt(0);
 
+            // Use switch statement to perform the corresponding action based on the chosen option
             switch (option) {
                 case 'a':
                 case 'A':
@@ -254,9 +282,9 @@ public class BandOfTheHour {
                 case 'p':
                 case 'P':
                     bandOfTheHour.printAssignment();
-            }
-
+            } // end of switch statement
         } while (option != 'X');
-    }
-}
+    } // end of main method
+
+} // end of BandOfTheHour class
 
