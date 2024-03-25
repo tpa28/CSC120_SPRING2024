c5ec2afa3023ca6d7842e5408c1bfe31ad22d56d
tpa28 <tpa29@miami.edu>
2024-03-25 13:24:20 -0400

    code

--- src/week11/Family.java
+++ src/week11/Family.java
@@ -3,23 +3,24 @@ package week11;
 public class Family {
     private Person[] family;
     private int numberOfPeople;
+    private static final int MAX_NUMBER_OF_MEMBERS = 10;
 
     // Default constructor
     public Family() {
-        this.family = new Person[10];
-        this.numberOfPeople = 0;
-    }
+        family = new Person[MAX_NUMBER_OF_MEMBERS];
+        numberOfPeople = 0;
+    } // end of default constructor
 
     // Mutator method to display each person within the Person array
     public void display() {
         for (int position = 0; position < numberOfPeople; position++) {
             System.out.println(family[position]);
         } // end of for loop
-    }
+    } // end of display method
 
     // Mutator method to add a person to the family
     public boolean addPerson(String name, int age) {
-        if (numberOfPeople < 10) {
+        if (numberOfPeople < MAX_NUMBER_OF_MEMBERS) {
             family[numberOfPeople] = new Person(name, age);
             numberOfPeople++;
             return true;
@@ -27,7 +28,7 @@ public class Family {
         else {
             return false; // No more space in the family
         } // end of if-else statement
-    }
+    } // end of addPerson method
 
     // Mutator method for a birthday
     public void birthday(String name) {
