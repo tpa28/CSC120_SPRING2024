cd0a7a10a419e408ba8d22f4448217d2e8f8b697
tpa28 <tpa29@miami.edu>
2024-03-25 12:51:10 -0400

    code

--- src/week11/Family.java
+++ src/week11/Family.java
@@ -1,53 +1,56 @@
 package week11;
 
 public class Family {
-    private Person[] members;
+    private Person[] family;
     private int numberOfPeople;
 
     // Default constructor
     public Family() {
-        this.members = new Person[10];
+        this.family = new Person[10];
         this.numberOfPeople = 0;
     }
 
+    // Mutator method to display each person within the Person array
     public void display() {
         for (int position = 0; position < numberOfPeople; position++) {
-            System.out.println(members[position]);
-        }
+            System.out.println(family[position]);
+        } // end of for loop
     }
 
     // Mutator method to add a person to the family
     public boolean addPerson(String name, int age) {
         if (numberOfPeople < 10) {
-            members[numberOfPeople] = new Person(name, age);
+            family[numberOfPeople] = new Person(name, age);
             numberOfPeople++;
             return true;
-        } else {
-            return false; // No more space in the family
         }
+        else {
+            return false; // No more space in the family
+        } // end of if-else statement
     }
 
     // Mutator method for a birthday
     public void birthday(String name) {
         for (int personInPosition = 0; personInPosition < numberOfPeople; personInPosition++) {
-            if (members[personInPosition].getName().equalsIgnoreCase(name)) {
-                members[personInPosition].incrementAge();
+            if (family[personInPosition].getName().equalsIgnoreCase(name)) {
+                family[personInPosition].incrementAge();
                 break;
-            }
-        }
-    }
+            } // end of if statement
+        } // end of for loop
+    } // end of birthday method
 
     // Accessor method to return the number of people
     public int getNumberOfPeople() {
         return numberOfPeople;
-    }
+    } // end of getNumberOfPeople method
 
     // Accessor method to return the total age of the people
     public int getTotalAge() {
         int totalAge = 0;
         for (int personInPosition = 0; personInPosition < numberOfPeople; personInPosition++) {
-            totalAge += members[personInPosition].getAge();
-        }
+            totalAge += family[personInPosition].getAge();
+        } // end of for loop
         return totalAge;
-    }
-}
+    } // end of getNumberOfPeople method
+
+} // end of Family class
--- src/week11/Person.java
+++ src/week11/Person.java
@@ -1,31 +1,38 @@
 package week11;
 
 public class Person {
-    private int age;
     private String name;
+    private int age;
 
+
+    // Default constructor
     public Person(){
         this.name = null;
         this.age = 0;
     }
 
+    // Constructor to accept name and age of a person
     public Person(String name, int age) {
-        this.age = age;
         this.name = name;
+        this.age = age;
     }
 
+    // toString for the output of a person
     public String toString(){
         return name + " is " + age + " years old";
     }
 
+    // Mutator method to increase the age by one
     public void incrementAge() {
         age++;
     }
 
+    // Accessor method to get the age
     public int getAge() {
         return age;
     }
 
+    // Accessor method to get the name
     public String getName() {
         return name;
     }
